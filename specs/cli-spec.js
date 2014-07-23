b88cc02fdf4b335cd7aea2b7087e8c1abc44138b
will chan <willchan2@gmail.com>
2014-07-23 08:07:28 -0400

    added a negativity test for retrieving command line arguments

--- specs/cli-spec.js
+++ specs/cli-spec.js
@@ -8,8 +8,16 @@ var cli = require('../lib/cli'),
   describe('cli package', function() {
     describe('.getCommandLineOptions', function() {
       it('returns an object where the property name and property value is option name and option value, respectively', function() {
-        var options = cli.getCommandLineOptions(['testCommand', '-testOpt', 'testOptValue']);
+        var options = cli.getCommandLineOptions(['testCommand', '-testOpt', 'testOptValue', '-anotherTestOpt', 'anotherTestOptValue']);
         assert.equal(options.testOpt, 'testOptValue');
+        assert.equal(options.anotherTestOpt, 'anotherTestOptValue');
+      });
+
+      it('should not return undefined property values for specified options without specified values', function() {
+        var options = cli.getCommandLineOptions(['testCommand', '-testOpt', '-anotherTestOpt']);
+        console.log("OPTIONS", options);
+        assert.equal(options.testOpt, undefined);
+        assert.equal(options.anotherTestOpt, undefined);
       });
     });
   });
